7a875096128048d7341c2c4eb94c914d74ecca77
unknown <danya.pestr@gmail.com>
2024-11-18 23:48:49 +0300

    first work

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(number_of_card: str) -> str:
+    """Маскирует номер банковской карты"""
+    if len(number_of_card) == 16 and number_of_card.isdigit():
+        masked_card = number_of_card[:5] + "******" + number_of_card[12:]
+        return masked_card
+    else:
+        print("Номер карты должен содержать 16 цифр")
+
+
+def get_mask_account(number_of_account: str) -> str:
+    """Маскирует номер счёта"""
+    if number_of_account.isdigit():
+        masked_account = "**" + number_of_account[3:]
+        return masked_account
+    else:
+        print("Неверный номер счёта")
