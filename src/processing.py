fff21fb4baf838cf72137c432142675f2727e7c6
Danila <danya.pestr@gmail.com>
2024-12-09 21:02:50 +0300

    fixes 1.02

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_of_dicts: list, parameter: str = 'EXECUTED') -> list:
+def filter_by_state(list_of_dicts: list, parameter: str = "EXECUTED") -> list:
     """Принимает список словарей и значение ключа state(по умолчанию - EXECUTED)
     Возвращает изменённый список словарей, у которых ключ state
     изменён на указанный ключ"""
@@ -6,14 +6,15 @@ def filter_by_state(list_of_dicts: list, parameter: str = 'EXECUTED') -> list:
     if isinstance(list_of_dicts, list) and isinstance(parameter, str):
         return [line for line in list_of_dicts if line.get("state") == parameter]
     else:
-        raise Exception('Некорректный тип данных')
+        raise Exception("Некорректный тип данных")
+
 
 def sort_by_date(list_of_dicts: list, is_sort: bool = True) -> list:
     """Принимает список словарей и значение сортировки(по умолчанию - True)
     Возвращает список, отсортированный по дате"""
 
     if isinstance(list_of_dicts, list) and isinstance(is_sort, bool):
-        result = sorted(list_of_dicts, key=lambda x: x.get('date'), reverse=is_sort)
+        result = sorted(list_of_dicts, key=lambda x: x.get("date"), reverse=is_sort)
         return result
     else:
-        raise Exception('Некорректный тип данных')
+        raise Exception("Некорректный тип данных")
