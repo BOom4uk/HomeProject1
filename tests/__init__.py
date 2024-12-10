23f8abc2e20a5da2b44afdff3e052a650dd34a66
Danila <danya.pestr@gmail.com>
2024-12-10 16:27:13 +0300

    fixes 1.04

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,18 @@
 def add_numbers(a, b):
     return a + b
 
+
 def is_even(a):
     return a % 2 == 0
+
 
 def find_max(my_list):
     if len(my_list) > 0:
         return max(my_list)
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     assert add_numbers(2, 2) == 4
 
     assert is_even(2) == True
