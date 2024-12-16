c4aa46472ae80c37cbb7318e8c8c45edec2013c5
Danila <danya.pestr@gmail.com>
2024-12-16 21:09:33 +0300

    fixes 1.08

--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,15 @@ def filter_by_state(list_of_dicts: list, parameter: str = "EXECUTED") -> list:
     """Принимает список словарей и значение ключа state(по умолчанию - EXECUTED)
     Возвращает изменённый список словарей, у которых ключ state
     изменён на указанный ключ"""
-
-    if isinstance(list_of_dicts, list) and isinstance(parameter, str):
-        return [line for line in list_of_dicts if line.get("state") == parameter]
+    score = 0
+    for keys in list_of_dicts:
+        if 'state' in keys:
+            score += 1
+    print(score)
+    if score == len(list_of_dicts) and score != 0:
+        if isinstance(list_of_dicts, list) and isinstance(parameter, str):
+            if len(list_of_dicts) >= 1:
+                return [line for line in list_of_dicts if line.get("state") == parameter]
     else:
         raise Exception("Некорректный тип данных")
 
@@ -12,9 +18,12 @@ def filter_by_state(list_of_dicts: list, parameter: str = "EXECUTED") -> list:
 def sort_by_date(list_of_dicts: list, is_sort: bool = True) -> list:
     """Принимает список словарей и значение сортировки(по умолчанию - True)
     Возвращает список, отсортированный по дате"""
-
-    if isinstance(list_of_dicts, list) and isinstance(is_sort, bool):
-        result = sorted(list_of_dicts, key=lambda x: x.get("date"), reverse=is_sort)
-        return result
-    else:
-        raise Exception("Некорректный тип данных")
+    score = 0
+    for keys in list_of_dicts:
+        if 'state' in keys:
+            score += 1
+        if score == len(list_of_dicts) and score != 0:
+            if isinstance(list_of_dicts, list) and isinstance(is_sort, bool):
+                result = sorted(list_of_dicts, key=lambda x: x.get("date"), reverse=is_sort)
+                return result
+    raise Exception("Некорректный тип данных")
